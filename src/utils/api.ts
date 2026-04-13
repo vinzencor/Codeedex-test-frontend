88da2ad2f2ee4badd1c47f3319d3cb48279e008c
vinzencor <rahulpradeepan55@gmail.com>
2026-04-13 16:41:43 +0530

    TEst

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend URL
+  baseURL: 'https://codeedex-test-backend.vercel.app/api', // Backend URL
   headers: {
     'Content-Type': 'application/json',
   },
